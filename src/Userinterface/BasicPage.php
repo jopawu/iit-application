83d5e3fcbb21fcc9928ceaacf95faadb1d806431
Björn Heyser <bh@bjoernheyser.de>
2021-08-25 15:34:25 +0200

    more basic structure

---
+++ src/Userinterface/BasicPage.php
@@ -0,0 +1,13 @@
+<?php
+
+/* Copyright (c) 1998-2019 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+namespace iit\Application\Userinterface;
+
+/**
+ * @author      Björn Heyser <info@bjoernheyser.de>
+ */
+class BasicPage
+{
+    protected $pageContent;
+}
---
+++ src/Userinterface/PageContent.php
@@ -0,0 +1,15 @@
+<?php
+
+/* Copyright (c) 1998-2019 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+namespace iit\Application\Userinterface;
+
+/**
+ * @author      Björn Heyser <info@bjoernheyser.de>
+ */
+class PageContent
+{
+    protected $header;
+    protected $body;
+    protected $footer;
+}
