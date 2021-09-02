5652e531a2d969e130934b6efecfba64270f2875
Björn Heyser <bh@bjoernheyser.de>
2021-09-02 11:24:03 +0200

    basic database abstraction

---
+++ src/Database/Database.php
@@ -0,0 +1,55 @@
+<?php
+
+/* Copyright (c) 1998-2019 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+namespace iit\Application\Database;
+
+/**
+ * @author      Björn Heyser <info@bjoernheyser.de>
+ */
+class Database
+{
+    /**
+     * @var \PDO
+     */
+    protected $connection;
+
+    /**
+     * @param string $hostname
+     * @param string $database
+     * @param string $username
+     * @param string $password
+     */
+    public function __construct($hostname, $database, $username, $password)
+    {
+        $options  = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => FALSE
+        ];
+
+        $dsn = "mysql:host={$hostname};dbname={$database};charset=utf8";
+        
+        $this->connection = new \PDO($dsn, $username, $password, $options);
+    }
+
+    public function select()
+    {
+
+    }
+
+    public function insert()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+}
