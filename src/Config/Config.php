bcb31fc17920c31c950cdaf95989d4115f115a65
Björn Heyser <bh@bjoernheyser.de>
2021-09-02 11:23:42 +0200

    ini config implementation

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -9,5 +9,56 @@ namespace iit\Application\Config;
  */
 class Config
 {
+    /**
+     * @var array
+     */
+    protected $iniSections;
+    
+    /**
+     * @param string $filename
+     */
+    protected function read($filename)
+    {
+        $reader = new Reader($filename);
+        $this->sections = $reader->read();
+    }
 
+    /**
+     * @param string $section
+     * @param string $variable
+     * @return string
+     */
+    public function getVariable($section, $variable)
+    {
+        $this->checkVariable($section, $variable);
+        return $this->iniSections[$section][$variable];
+    }
+
+    /**
+     * @param string $section
+     * @param string $variable
+     */
+    protected function checkVariable($section, $variable)
+    {
+        if( !isset($this->iniSections[$section]) )
+        {
+            throw new \InvalidArgumentException("ini section does not exist: {$section}");
+        }
+
+        if( !isset($this->iniSections[$section][$variable]) )
+        {
+            throw new \InvalidArgumentException("ini variable does not exist: {$variable}");
+        }
+    }
+
+    /**
+     * @param string $filename
+     * @return Config
+     */
+    public static function fromFile($filename)
+    {
+        $config = new self();
+        $config->read($filename);
+        return $config;
+    }
 }
--- src/Config/Reader.php
+++ src/Config/Reader.php
@@ -9,5 +9,48 @@ namespace iit\Application\Config;
  */
 class Reader
 {
+    /**
+     * @var string
+     */
+    protected $filename;
 
+    /**
+     * @param string $filename
+     */
+    public function __construct($filename)
+    {
+        $this->filename = $filename;
+    }
+
+    public function read()
+    {
+        $this->checkFile();
+
+        $iniSections = parse_ini_file($this->filename);
+
+        if( $iniSections === false )
+        {
+            throw new \InvalidArgumentException("could not parse config file: {$this->filename}");
+        }
+
+        return $iniSections;
+    }
+
+    protected function checkFile()
+    {
+        if( !file_exists($this->filename) )
+        {
+            throw new \InvalidArgumentException("config file does not exist: {$this->filename}");
+        }
+
+        if( !is_file($this->filename) )
+        {
+            throw new \InvalidArgumentException("config file is not a file: {$this->filename}");
+        }
+
+        if( !is_readable($this->filename) )
+        {
+            throw new \InvalidArgumentException("config file is not readable: {$this->filename}");
+        }
+    }
 }
