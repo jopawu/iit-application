ccea9a8c4c64f9078b2ce71405cb6ba514b562b3
Björn Heyser <bh@bjoernheyser.de>
2023-09-17 19:52:58 +0200

    locale independent currency formatter based on currencies definition file

--- src/Formatter/Currency.php
+++ src/Formatter/Currency.php
@@ -13,7 +13,40 @@ class Currency extends Formatter
 {
     use AssertionTrait;
 
-    const LOCALE = 'de_DE';
+    const LOCALE_PHP_FORMATTER = 'de_DE';
+    const USE_PHP_FORMATTER = false;
+
+    /**
+     * @var bool
+     */
+    protected $unitSymbolAlwaysFirst = false;
+
+    /**
+     * @var bool
+     */
+    protected $unitSymbolAlwaysLast = false;
+
+    /**
+     * @return Currency
+     */
+    public function withUnitSymbolAlwaysFirst()
+    {
+        $clone = clone $this;
+        $clone->unitSymbolAlwaysFirst = true;
+        $clone->unitSymbolAlwaysLast = false;
+        return $clone;
+    }
+
+    /**
+     * @return Currency
+     */
+    public function withUnitSymbolAlwaysLast()
+    {
+        $clone = clone $this;
+        $clone->unitSymbolAlwaysFirst = false;
+        $clone->unitSymbolAlwaysLast = true;
+        return $clone;
+    }
 
     /**
      * @param mixed $balance
@@ -24,21 +57,21 @@ class Currency extends Formatter
         /* @var Balance $balance */
         $this->assertInstanceOf($balance, [Balance::class]);
 
-        if(true)
+        if( self::USE_PHP_FORMATTER )
         {
-            return $this->usingMyFormatted($balance);
+            return $this->usingPhpNumberFormatted($balance);
         }
 
-        return $this->usingNumberFormatted($balance);
+        return $this->usingMyFormatted($balance);
     }
 
     /**
      * @param Balance $balance
      * @return false|string
      */
-    public function usingMyFormatted(Balance $balance)
+    protected function usingMyFormatted(Balance $balance)
     {
-        if( $balance->getCurrency()->isSymbolFirst() )
+        if( $this->isSymbolFirst($balance) )
         {
             return $balance->getCurrency()->getUnitSymbol() . ' ' . $this->formatValue($balance);
         }
@@ -48,20 +81,93 @@ class Currency extends Formatter
 
     /**
      * @param Balance $balance
+     * @return bool
+     */
+    protected function isSymbolFirst(Balance $balance)
+    {
+        if( $this->unitSymbolAlwaysLast )
+        {
+            return false;
+        }
+
+        return $balance->getCurrency()->isSymbolFirst();
+    }
+
+    /**
+     * @param Balance $balance
      * @return string
      */
     protected function formatValue(Balance $balance) : string
     {
-        list($int, $dec) = explode('.', $balance->getDecimal())
+        $sign = $balance->isNegative() ? '-' : '';
+        $positiveFloat = $sign ? $balance->withNegatedValue()->getDecimal() : $balance->getDecimal();
+
+        list($integerPart, $decimalPart) = explode('.', $positiveFloat);
+
+        $integerPartThousandsSeparated = $this->buildThousandsSeparatedIntegerPart(
+            $integerPart, $balance->getCurrency()->getThousandsSeparator()
+        );
+
+        $decimalPartDigitCompleted = $this->buildDigitCompletedDecimalPart(
+            $decimalPart, $balance->getCurrency()->getDecimalPrecision()
+        );
+
+        $value = $sign . $integerPartThousandsSeparated;
+        $value .= $balance->getCurrency()->getDecimalMark();
+        $value .= $decimalPartDigitCompleted;
+
+        return $value;
+    }
+
+    /**
+     * @param string $integer
+     * @param string $thousendsSeparator
+     * @return string
+     */
+    protected function buildThousandsSeparatedIntegerPart(string $integer, string $thousendsSeparator)
+    {
+        if( strlen($integer) > 3 )
+        {
+            $intSeparated = '';
+
+            while( strlen($integer) > 3 )
+            {
+                $intSeparated = substr($integer, -3, 3) . $intSeparated;
+                $intSeparated = $thousendsSeparator . $intSeparated;
+
+                $integer = substr($integer, 0, strlen($integer) - 3);
+            }
+
+            return $integer . $intSeparated;
+        }
+
+        return (string)$integer;
+    }
+
+    /**
+     * @param string|null $decimalPart
+     * @param int $decimalPrecision
+     * @return string
+     */
+    protected function buildDigitCompletedDecimalPart(?string $decimalPart, int $decimalPrecision)
+    {
+        while( strlen($decimalPart) < $decimalPrecision )
+        {
+            $decimalPart .= '0';
+        }
+
+        return $decimalPart;
     }
 
     /**
      * @param Balance $balance
      * @return false|string
      */
-    public function usingNumberFormatted(Balance $balance)
+    protected function usingPhpNumberFormatted(Balance $balance)
     {
-        $formatter = NumberFormatter::create(self::LOCALE, NumberFormatter::CURRENCY);
+        $formatter = NumberFormatter::create(
+            self::LOCALE_PHP_FORMATTER, NumberFormatter::CURRENCY
+        );
 
         return $formatter->format(
             $balance->getValue() / $balance->getCurrency()->getSubunitFactor()
